bf5f7d261c8bcd4698661dccbd71acb4b0180737
OneXyedKing <201926227+OneXyedKing@users.noreply.github.com>
2025-04-15 15:31:22 +0200

    Attempt completing exercise.
    Got some of it done but got NaN, will continue later.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,40 @@
-const reverseString = function() {
+/*
+Get input
+Turn input into array
+Reverse array
+Check and store array length newArray.length
+Print array as string
+*/
 
+
+const reverseString = function(userString) {
+    const newArray = userString.split("");
+    const arrayLength = newArray.length;
+    const reverseArray = [];
+    let i = 0;
+
+    while (i < arrayLength) {
+        i++
+        reverseArray.push(newArray-i);
+    }
+
+    return reverseArray.join();
 };
 
+//Test
+const myInput = "Apples.";
+console.log(reverseString(myInput));
+
+
+/*
+const myString = "I like apples.";
+const newArray = myString.split("");
+const maxLength = newArray.length;
+const decrementer = 0;
+
+console.log(maxLength);
+*/
+//console.log(newArray[newArray.length-1]);
+
 // Do not edit below this line
-module.exports = reverseString;
+module.exports = reverseString
