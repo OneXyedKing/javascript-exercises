30ff58ea303f35e6c8698e878a5c7ad9f079c22b
OneXyedKing <201926227+OneXyedKing@users.noreply.github.com>
2025-04-15 09:57:54 +0200

    Complete repeatString task

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    const output = []; // Create array INSIDE function
+    let i = 0;
+    
+    while (i < times) {
+        output.push(word); // Actually add the input word
+        i++;
+    }
+    return output;
 };
 
-// Do not edit below this line
-module.exports = repeatString;
+// Test case
+const inputWord = prompt("Enter a Word");
+const repetitions = parseInt(prompt("How many repitions?"));
+const resultArray = repeatString(inputWord, repetitions);
+
+console.log(resultArray); // ["apple", "apple", "apple", "apple", "apple"]
+console.log(resultArray.join('')); // "appleappleappleappleapple"
